d14cd6e18206b3f3aa7224da9b667b41bab3bb7a
Francisco Marcet <58398541+fcomarcet1@users.noreply.github.com>
2021-06-10 20:49:03 +0200

    Add: config .env, route prefix /api

---
+++ src/Controller/Api/VideoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VideoController extends AbstractController
+{
+    /**
+     * @Route("/video", name="video")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/VideoController.php',
+        ]);
+    }
+}
